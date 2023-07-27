32aa64bda2f9bd34e4ab50d9dd479e17a8e50608
Frederik Leys <frederik.leys@gmail.com>
2023-07-28 00:17:37 +0200

    Add new feature X

--- public/controller.js
+++ public/controller.js
@@ -1,5 +1,4 @@
 let socket = io.connect('https://desolate-depths-29424-e1ff0b4f81bf.herokuapp.com');
-let channel;
 let pc = new RTCPeerConnection();
 let remoteVideo = document.getElementById('remote-video');
 
@@ -15,22 +14,6 @@ pc.onicecandidate = ({candidate}) => {
     }
 };
 
-socket.on('offer', async (offer) => {
-    pc.ondatachannel = (event) => {
-        channel = event.channel;
-        channel.onmessage = (event) => {
-            document.getElementById('messages').innerText += '\n' + event.data;
-        };
-    };
-
-    await pc.setRemoteDescription(offer);
-
-    const answer = await pc.createAnswer();
-    await pc.setLocalDescription(answer);
-
-    socket.emit('answer', answer);
-});
-
 socket.on('answer', (answer) => {
     pc.setRemoteDescription(answer);
 });
@@ -39,16 +22,9 @@ socket.on('candidate', (candidate) => {
     pc.addIceCandidate(candidate);
 });
 
-document.getElementById('message-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const message = document.getElementById('message-input').value;
-    document.getElementById('message-input').value = '';
-    channel.send(message);
-});
-
 document.getElementById('connect').addEventListener('click', async () => {
-    channel = pc.createDataChannel('chat');
-    channel.onmessage = (event) => {
-        document.getElementById('messages').innerText += '\n' + event.data;
-    };
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+
+    socket.emit('offer', offer);
 });
--- public/robot.js
+++ public/robot.js
@@ -1,68 +1,34 @@
 let socket = io.connect('https://desolate-depths-29424-e1ff0b4f81bf.herokuapp.com');
-let localVideo = document.getElementById('local-video');
-let channel;
+let pc = new RTCPeerConnection();
+let localStream;
 
-let stream;
 navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(s => {
-        stream = s;
-        localVideo.srcObject = stream;
+    .then(stream => {
+        localStream = stream;
+
+        localStream.getTracks().forEach(track => {
+            pc.addTrack(track, localStream);
+        });
+
+        pc.onicecandidate = ({candidate}) => {
+            if (candidate) {
+                socket.emit('candidate', candidate);
+            }
+        };
+
+        socket.on('offer', async (offer) => {
+            await pc.setRemoteDescription(offer);
+
+            const answer = await pc.createAnswer();
+            await pc.setLocalDescription(answer);
+
+            socket.emit('answer', answer);
+        });
+
+        socket.on('candidate', (candidate) => {
+            pc.addIceCandidate(candidate);
+        });
     })
     .catch(error => {
         console.error('Error accessing media devices.', error);
     });
-
-
-let pc = new RTCPeerConnection();
-
-// Add the video track to the peer connection
-if (stream) {
-    stream.getTracks().forEach(track => pc.addTrack(track, stream));
-}
-
-pc.onicecandidate = ({candidate}) => {
-    socket.emit('candidate', candidate);
-};
-
-document.getElementById('connect').addEventListener('click', async () => {
-    channel = pc.createDataChannel('chat');
-    channel.onmessage = (event) => {
-        document.getElementById('messages').innerText += '\n' + event.data;
-    };
-
-    const offer = await pc.createOffer();
-    await pc.setLocalDescription(offer);
-
-    socket.emit('offer', offer);
-});
-
-socket.on('offer', async (offer) => {
-    pc.ondatachannel = (event) => {
-        channel = event.channel;
-        channel.onmessage = (event) => {
-            document.getElementById('messages').innerText += '\n' + event.data;
-        };
-    };
-
-    await pc.setRemoteDescription(offer);
-
-    const answer = await pc.createAnswer();
-    await pc.setLocalDescription(answer);
-
-    socket.emit('answer', answer);
-});
-
-socket.on('answer', (answer) => {
-    pc.setRemoteDescription(answer);
-});
-
-socket.on('candidate', (candidate) => {
-    pc.addIceCandidate(candidate);
-});
-
-document.getElementById('message-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const message = document.getElementById('message-input').value;
-    document.getElementById('message-input').value = '';
-    channel.send(message);
-});
